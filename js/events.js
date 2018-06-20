0ca17eb632354f2179cae556c15921ecf8fa6232
chejianchao <820992376@qq.com>
2018-06-20 06:09:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
 
+function getIt(){
+  $("p").on("click", function(e){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(e){
+    
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+  
 });
